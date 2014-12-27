ad328c7245957645c473df651628306e587a749e
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-12-27 13:54:22 -0800

    about half way through

---
+++ app/controllers/friends/edit.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions: {
+      cancel: function() {
+        this.transitionToRoute('friends.show', this.get('model'));
+        return false;
+      }
+    }
+});
--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,26 +1,11 @@
-import Ember from 'ember';
+import FriendsBaseController from './base';
 
-export default Ember.Controller.extend({
-  isValid: Ember.computed(
-    'model.email',
-    'model.firstName',
-    'model.lastName',
-    'model.twitter',
-    function() {
-      return !Ember.isEmpty(this.get('model.email')) &&
-        !Ember.isEmpty(this.get('model.firstName')) &&
-        !Ember.isEmpty(this.get('model.lastName')) &&
-        !Ember.isEmpty(this.get('model.twitter'));
-    }
-  ),
+export default FriendsBaseController.extend({
   actions: {
-    save: function() {
-      console.log("+- save action in friends new controller");
-      return true;
-    },
     cancel: function() {
-      console.log("+- cancel action in friends new controller");
-      return true;
+      this.transitionToRoute('friends');
+
+      return false;
     }
   }
 });
